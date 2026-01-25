70839cd13909bbea0070425f72a562f7c283e544
Vigneshkumartcse <vigneshkumar.t2022csec@sece.ac.in>
2026-01-25 10:01:49 +0530

    newchanges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "./CartContext";
-import "./../styles/Navbar.css";
+import "../styles/Navbar.css";
 
 function Navbar({ searchQuery, setSearchQuery, onCartClick }) {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
